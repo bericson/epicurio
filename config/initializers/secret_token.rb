3ccbc157a8840c7a1ffabbf0cdf9823f193032c9
Ben E <benbjammin@gmail.com>
2012-12-08 14:58:54 -0700

    Initial commit prior to created new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epicurio::Application.config.secret_token = 'f350658019233eed65dbc81d62752838fd4dceced8d4c907b82c9544a3c593a8d0e1a6a714765dd3a3d7645b36b9d947c6b59303c5a35dd7aad2a8597fcf39e6'
