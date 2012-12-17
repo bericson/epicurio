a0d0792f76bec4faf700432bec31ac4d46fa1641
Ben E <benbjammin@gmail.com>
2012-12-17 13:25:23 -0700

    Enhanced the homepage slider to have thumbnail images as selector buttons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,8 @@ class HomeController < ApplicationController
 
   def index
     @recipes = Recipe.all
+    @recipes_random_limited = Recipe.all(:limit => 14)
+    #@recipes_limited = Recipe.all.sample(14)
   end
-
-  #def index
-  #  @users = User.all
-  #end
 
 end
