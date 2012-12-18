346f08ff846d5f1afe872f633ac44f170d356ce4
Ben E <benbjammin@gmail.com>
2012-12-17 17:07:53 -0700

    Modify devise mailer files to work on Heroku (hoepfully)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
 Epicurio::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.action_mailer.default_url_options = { :host => 'herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'fierce-shelf-8580.herokuapp.com' }
+  #config.action_mailer.default_url_options = { :host => 'herokuapp.com' }
 # ActionMailer Config
 # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
